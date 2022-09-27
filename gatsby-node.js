7e1de509dd1bfdbbad3ebd0e7db9858e0f95990f
Enaium <wyy-666@hotmail.com>
2022-09-27 17:24:35 +0800

    fix url

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,7 +62,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode }).replace("#", "-")
     createNodeField({
       name: `slug`,
       node,
